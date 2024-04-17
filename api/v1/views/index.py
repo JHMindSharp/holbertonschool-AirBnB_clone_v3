61d8ef124d1f1340132f8c7088da1760f3478037
Miniknacky <Jonathanhuybrechts@gmail.com>
2024-04-17 17:39:12 +0200

    task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
 """
-Module for index view.
+Index view for API v1
 """
+
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def api_status():
-    """
-    Return the status of the API.
-    """
-    return jsonify({"status": "OK"})
+@app_views.route('/api/v1/stats', methods=['GET'])
+def get_stats():
+    """Retrieves the number of each object by type"""
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
