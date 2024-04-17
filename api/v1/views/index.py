da2bafdc350b7419a84d90ad394c3aa1d926b44a
Miniknacky <Jonathanhuybrechts@gmail.com>
2024-04-17 17:32:18 +0200

    task 4 maj

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,7 @@ Module for index view.
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def api_status():
     """
