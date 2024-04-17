8e77e4ed5e583775c797cf08d4c118d84d63b167
Miniknacky <Jonathanhuybrechts@gmail.com>
2024-04-17 16:18:14 +0200

    task 1

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Test BaseModel for expected behavior and documentation"""
-from datetime import datetime
+from datetime import datetime, timedelta
 import inspect
 import models
-import pep8 as pycodestyle
+import pycodestyle
 import time
 import unittest
 from unittest import mock
@@ -79,22 +79,23 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(inst.number, 89)
 
     def test_datetime_attributes(self):
-        """Test that two BaseModel instances have different datetime objects
-        and that upon creation have identical updated_at and created_at
-        value."""
-        tic = datetime.now()
+        """Test that two BaseModel instances have different datetime
+        objects."""
         inst1 = BaseModel()
-        toc = datetime.now()
-        self.assertTrue(tic <= inst1.created_at <= toc)
-        time.sleep(1e-4)
-        tic = datetime.now()
+        time.sleep(0.01)
         inst2 = BaseModel()
-        toc = datetime.now()
-        self.assertTrue(tic <= inst2.created_at <= toc)
-        self.assertEqual(inst1.created_at, inst1.updated_at)
-        self.assertEqual(inst2.created_at, inst2.updated_at)
-        self.assertNotEqual(inst1.created_at, inst2.created_at)
-        self.assertNotEqual(inst1.updated_at, inst2.updated_at)
+
+        self.assertNotEqual(
+            inst1.created_at, inst2.created_at, "inst1.created_at"
+            "should not be equal to inst2.created_at")
+
+        self.assertEqual(
+            inst1.created_at, inst1.updated_at, "For inst1, created_at"
+            "should be equal to updated_at upon creation")
+
+        self.assertEqual(
+            inst2.created_at, inst2.updated_at, "For inst2, created_at"
+            "should be equal to updated_at upon creation")
 
     def test_uuid(self):
         """Test that id is a valid uuid"""
