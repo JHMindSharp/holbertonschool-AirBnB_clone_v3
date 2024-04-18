2020e6ef5fe77e01a387aa5e550ab27e92aa1509
Miniknacky <Jonathanhuybrechts@gmail.com>
2024-04-18 21:10:59 +0200

    task 9 pycode

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -7,11 +7,14 @@ from models import storage
 from models.amenity import Amenity
 from api.v1.views import app_views
 
+
 @app_views.route('/amenities', methods=['GET'], strict_slashes=False)
 def get_amenities():
     """Retrieves the list of all Amenity objects"""
-    amenities = [amenity.to_dict() for amenity in storage.all(Amenity).values()]
+    amenities = [amenity.to_dict() for amenity in storage.all(Amenity).values(
+    )]
     return jsonify(amenities)
+
 
 @app_views.route('/amenities/<amenity_id>', methods=['GET'])
 def get_amenity(amenity_id):
@@ -20,6 +23,7 @@ def get_amenity(amenity_id):
     if not amenity:
         abort(404)
     return jsonify(amenity.to_dict())
+
 
 @app_views.route('/amenities', methods=['POST'], strict_slashes=False)
 def create_amenity():
@@ -32,6 +36,7 @@ def create_amenity():
     amenity = Amenity(**data)
     amenity.save()
     return jsonify(amenity.to_dict()), 201
+
 
 @app_views.route('/amenities/<amenity_id>', methods=['PUT'])
 def update_amenity(amenity_id):
@@ -48,6 +53,7 @@ def update_amenity(amenity_id):
             setattr(amenity, key, value)
     amenity.save()
     return jsonify(amenity.to_dict()), 200
+
 
 @app_views.route('/amenities/<amenity_id>', methods=['DELETE'])
 def delete_amenity(amenity_id):
